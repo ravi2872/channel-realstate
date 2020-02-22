fa334ccb1135ddb636474cb2b4f2f7e4c318b5d7
Ravikumar Anumalasetty <ravikumar.asvs@gmail.com>
2020-02-22 16:03:26 +0530

    Initial Commit

---
+++ channel-realstate/src/main/java/com/channel/realestate/ChannelRealstateApplication.java
@@ -0,0 +1,13 @@
+package com.channel.realestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChannelRealstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChannelRealstateApplication.class, args);
+	}
+
+}
